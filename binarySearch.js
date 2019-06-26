21e3e95a45f6961d7a89ee92bf8bbce50ba042e2
Daniel Xu <dxu429@gmail.com>
2019-06-26 00:30:58 -0700

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -3,12 +3,12 @@ function binarySearch(arr, val) {
   let r = arr.length - 1
   while(l <= r) {
     let m = (l + r)/2
+    if(arr[m] === val)
+      return m
     if(arr[m] < val)
       l = m + 1
     else if(arr[m] > val)
       r = m - 1
-    else
-      return m
   }
   return -1
 }
