e2ebd413e50b38f38c6da5016ccd12b83fde0e68
Daniel Xu <dxu429@gmail.com>
2019-07-29 22:32:35 -0700

    Rename variables for maxOverlappingIntervals.js

--- maxOverlappingIntervals.js
+++ maxOverlappingIntervals.js
@@ -1,23 +1,24 @@
 import zip from './zip.js'
 function maxOverlappingIntervals(intervals) {
-  let [maxStart, maxEnd] = intervals.reduce(([start,end], [s,e]) => [Math.max(start, s), Math.max(end, e)], [-Infinity, -Infinity])
-  let maxTime = Math.max(maxStart, maxEnd)
+  const [maxStart, maxEnd] = intervals.reduce(([start,end], [s,e]) => [Math.max(start, s), Math.max(end, e)], [-Infinity, -Infinity])
+  const maxTime = Math.max(maxStart, maxEnd)
   let enterExit = Array(maxTime + 2).fill(0)
   for(let i = 0; i < intervals.length; ++i) {
     ++enterExit[intervals[i][0]]
     --enterExit[intervals[i][1]+1]
   }
-  let cur = 0
-  let idx = 0
-  let time = -Infinity
-  for(let i = 0; i <= maxTime; ++i) {
-    cur += enterExit[i]
-    if(time < cur) {
-      time = cur
-      idx = i
+  let currentlyOverlappingIntervals = 0
+  let time = 0
+  let maxOverlappingIntervalsSoFar = -Infinity
+  for(let startTime = 0; startTime <= maxTime; ++startTime) {
+    currentlyOverlappingIntervals += enterExit[startTime]
+    if(maxOverlappingIntervalsSoFar < currentlyOverlappingIntervals) {
+      maxOverlappingIntervalsSoFar = currentlyOverlappingIntervals
+      time = startTime
     }
   }
-  return idx
+  console.log(`Max intervals is ${maxOverlappingIntervalsSoFar} at time ${time}`)
+  return time
 }
 
 let start = [13, 28, 29, 14, 40, 17, 3]
